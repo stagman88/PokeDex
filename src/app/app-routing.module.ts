e8089004e0017838a08d796c0528760fe564f689
stagman88 <56920378+stagman88@users.noreply.github.com>
2021-03-26 14:01:45 +0000

    version angular updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes),
+    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }),
     CommonModule
   ],
   exports: [RouterModule]
